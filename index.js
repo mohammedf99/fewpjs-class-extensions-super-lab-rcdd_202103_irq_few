79fb5c0cefad579647eb0b3ff566ee6687e40a8d
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-01 01:31:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Deciduous extends Tree {
   }
   
   static definition() {
-    return `${super.definition()} branches and leaves. Deciduous trees shed their leaves annually.`
+    return `${super.definition()} Deciduous trees shed their leaves annually.`
   }
 }
 
